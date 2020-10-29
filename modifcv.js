5b8685d4223810df7602e088d91bd0c283d1e910
Imane BOUZIDI <imane.bbouzidi@gmail.com>
2020-10-29 18:20:00 +0100

    COMMIT8

--- modifcv.js
+++ modifcv.js
@@ -1,4 +1,19 @@
 
-   function fct1(){
-      document.getElementById("c1").style.display="block";
-      }
+
+    function fct2() {
+          document.getElementById("c2").style.display = "block";
+          document.getElementById("c3").style.display = "none";
+          document.getElementById("c4").style.display = "none";
+        }
+    function fct3() {
+          document.getElementById("c3").style.display = "block";
+          document.getElementById("c2").style.display = "none";
+          document.getElementById("c4").style.display = "none";
+        }
+    function fct4() {
+          document.getElementById("c4").style.display = "block";
+          document.getElementById("c2").style.display = "none";
+          document.getElementById("c3").style.display = "none";
+        }
+    
+   
